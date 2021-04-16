09385397e846a82d4b38ad5f4f04391e0a7fcc00
sneha025 <simaurya96@gmail.com>
2021-04-16 10:28:45 +0530

    created schema

--- Model/user.js
+++ Model/user.js
@@ -1,13 +1,20 @@
 const mongoose = require("mongoose");
-
+const { v4: uuidv4 } = require("uuid");
 //defining the schema
 const Schema = mongoose.Schema;
 
 const UserModel = new Schema({
-  userId: "",
-  username: "",
-  firstname: "",
-  lastname: "",
-  email: "",
-  password: "",
+  userId: uuidv4(),
+  userName: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  confirmPassword: { type: String, required: true },
 });
+
+
+// Compile model from schema
+//export model 
+//name,Model,collection
+module.exports =mongoose.model('user',UserModel,'users')
